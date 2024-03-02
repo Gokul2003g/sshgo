7caff0035216127e7b83144d5d59eae1fde7331d
Gokul2003g <75162131+Gokul2003g@users.noreply.github.com>
2024-03-02 14:58:06 +0530

    Changed project folder structure

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,22 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//     format!("Hello, {}! You've been greeted from Rust!", name)
+// }
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn password_auth(username: &str, password: &str) {
+    println!(
+        "The entered username: {} and password: {}",
+        username, password
+    );
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![password_auth])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { UnderlineTabs } from "./components/elements/Tab";
 // import './App.css'
 
 function App() {
-
   return (
-    <div className="m-0 bg-gray-900 text-2xl text-white h-screen w-screen flex flex-col  items-center">
+    <div className="m-0 bg-gray-900 text-2xl text-white h-screen w-screen flex flex-col items-center">
       <UnderlineTabs />
     </div>
   );
