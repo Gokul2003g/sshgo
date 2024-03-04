0e147cd3eb5db17b473e8a49f43e4f045dd04589
Gokul2003g <75162131+Gokul2003g@users.noreply.github.com>
2024-03-04 14:12:26 +0530

    Removed password input box.

--- src/pages/PasswordAuth.jsx
+++ src/pages/PasswordAuth.jsx
@@ -4,10 +4,9 @@ import React, { useState } from "react";
 
 const PasswordAuth = () => {
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
 
   async function password_auth() {
-    await invoke("password_auth", { username, password });
+    await invoke("password_auth", { username });
   }
 
   return (
@@ -24,12 +23,6 @@ const PasswordAuth = () => {
           placeholder="user@ipaddress"
           onChange={(e) => setUsername(e.currentTarget.value)}
           className="p-4 bg-transparent border-2 rounded-lg border-gray-500 text-white focus:border-gray-900"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.currentTarget.value)}
-          className=" p-4 bg-transparent border-2 rounded-lg border-gray-500 text-white focus:border-gray-900"
         />
         <Button type="submit" color="blue" ripple="light">
           Connect
