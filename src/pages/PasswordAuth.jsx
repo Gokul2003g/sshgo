8976b023f1a108f22f00c6aec04feb378baf914b
Gokul2003g <75162131+Gokul2003g@users.noreply.github.com>
2024-03-04 13:45:22 +0530

    Password based auth.

--- src/pages/PasswordAuth.jsx
+++ src/pages/PasswordAuth.jsx
@@ -4,10 +4,10 @@ import React, { useState } from "react";
 
 const PasswordAuth = () => {
   const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  // const [password, setPassword] = useState("");
 
   async function password_auth() {
-    await invoke("password_auth", { username, password });
+    await invoke("password_auth", { username });
   }
 
   return (
@@ -25,12 +25,12 @@ const PasswordAuth = () => {
           onChange={(e) => setUsername(e.currentTarget.value)}
           className="p-4 bg-transparent border-2 rounded-lg border-gray-500 text-white focus:border-gray-900"
         />
-        <input
-          type="password"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.currentTarget.value)}
-          className=" p-4 bg-transparent border-2 rounded-lg border-gray-500 text-white focus:border-gray-900"
-        />
+        {/* <input */}
+        {/*   type="password" */}
+        {/*   placeholder="Password" */}
+        {/*   onChange={(e) => setPassword(e.currentTarget.value)} */}
+        {/*   className=" p-4 bg-transparent border-2 rounded-lg border-gray-500 text-white focus:border-gray-900" */}
+        {/* /> */}
         <Button type="submit" color="blue" ripple="light">
           Connect
         </Button>
